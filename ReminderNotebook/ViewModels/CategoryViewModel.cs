d352222e1db98f7fccf523ac79ef305cd0fd6b35
Iryna Hrytsenko <ipz244_gis@student.ztu.edu.ua>
2026-05-12 23:32:49 +0300

    Categories: Implement business logic and ViewModel

---
+++ ReminderNotebook/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ReminderNotebook.Models;
+
+namespace ReminderNotebook.ViewModels;
+
+public class CategoryViewModel : BaseViewModel
+{
+    private int _id;
+    private string _name = string.Empty;
+    private string _colorHex = "#607D8B";
+
+    public int Id
+    {
+        get => _id;
+        set => SetField(ref _id, value);
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => SetField(ref _name, value);
+    }
+
+    public string ColorHex
+    {
+        get => _colorHex;
+        set => SetField(ref _colorHex, value);
+    }
+
+    public static CategoryViewModel FromModel(Category category) => new()
+    {
+        Id = category.Id,
+        Name = category.Name,
+        ColorHex = category.ColorHex
+    };
+
+    public Category ToModel() => new()
+    {
+        Id = Id,
+        Name = Name,
+        ColorHex = ColorHex
+    };
+}
