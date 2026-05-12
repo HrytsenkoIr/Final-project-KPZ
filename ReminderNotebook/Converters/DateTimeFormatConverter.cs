8bfb567d3b99ad89304c924ab20508908bdee419
Iryna Hrytsenko <ipz244_gis@student.ztu.edu.ua>
2026-05-12 23:28:05 +0300

    Core: Add MVVM helpers and XAML converters

---
+++ ReminderNotebook/Converters/DateTimeFormatConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ReminderNotebook.Converters;
+
+public class DateTimeFormatConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not DateTime dt) return string.Empty;
+
+        var format = parameter as string ?? "dd.MM.yyyy HH:mm";
+        return dt.ToString(format, new CultureInfo("uk-UA"));
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new NotSupportedException();
+}
